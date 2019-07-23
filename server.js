540f54cbdbbb7fe732ee7fc608b19b026841f123
Shadab420 <shadab_anwar@outlook.com>
2019-07-23 17:44:35 +0600

    Added necessary middlewares and packages to server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+//middleware
+app.use(morgan('dev')); //morgan used to log requests
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false})); //for handling form data
+app.use(bodyParser.json()); //for handling json data.
+
+app.get('/', (req,res) => {
+    return res.send("hello");
+})
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server running ${PORT}`);
+
+    mongoose.connect("mongodb://localhost/money_mgmt", () => {
+        console.log("database connected!");
+    });
+});
