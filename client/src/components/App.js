1ee46d6eea22d520f3fcb056a8a10f84bb4638d4
Shadab420 <shadab_anwar@outlook.com>
2019-07-26 21:10:29 +0600

    Added React-Bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="App">
-      <h1>PubG</h1>
-    </div>
+  	<BrowserRouter>
+  		<Container>
+		    <div className="App">
+		      
+		      <Switch>
+		      	<Route path="/" component = {Home} exact/>
+		      </Switch>
+
+		    </div>
+	    </Container>
+	 </BrowserRouter>
   );
 }
 
